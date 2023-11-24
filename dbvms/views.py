83775fc5d3f2af3ca5f565ba77d58bd531c44e26
Rodney <rodney002mwanje@gmail.com>
2023-11-24 10:17:49 +0300

    feat: Teacher Confirmation of pickups

--- dbvms/views.py
+++ dbvms/views.py
@@ -6,6 +6,7 @@ from .serializers import EventSerializer
 from .models import Event
 from datetime import datetime
 from Administrator.serializers import MessageSerializer
+from Administrator.models import Users
 
 # Create your views here.
 class RegisterDBVMS(APIView):
@@ -61,6 +62,68 @@ class ConfirmParentPickUps(APIView):
             return Response(serializer.data, status=status.HTTP_403_FORBIDDEN)
         
         atendee[0].prnt_cfrm_dropped = datetime.now()
+        atendee[0].save()
+
+        data = {"message": "SuccessFully Updated"}
+        serializer = MessageSerializer(data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class ConfirmTeacherPickup(APIView):
+    """
+    Used to handle Pickups for kids by Teacher
+    """
+    def post(self, request, *args, **kwargs):
+        """
+        Used by teacher to confirm the kid has arrived to the event
+        """
+        parent_email = request.data.get("parent_email")
+
+        atendee = Event.objects.filter(parent_email=parent_email)
+
+        teacher_id = request.data.get("teacher_id")
+        teacher = Users.objects.filter(id=teacher_id)
+
+
+        if not atendee or not teacher:
+            data = {"message": "Invalid Data"}
+            serializer = MessageSerializer(data)
+            return Response(serializer.data, status=status.HTTP_403_FORBIDDEN)
+        
+        if atendee[0].teacher_id != teacher.id:
+            data = {"message": "Invalid Data"}
+            serializer = MessageSerializer(data)
+            return Response(serializer.data, status=status.HTTP_403_FORBIDDEN)
+                
+        atendee[0].tchr_cfrm_received = datetime.now()
+        atendee[0].save()
+
+        data = {"message": "SuccessFully Updated"}
+        serializer = MessageSerializer(data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, *args, **kwargs):
+        """
+        Used by teacher to confirm the kid is returned from the event
+        """
+        parent_email = request.data.get("parent_email")
+
+        atendee = Event.objects.filter(parent_email=parent_email)
+
+        teacher_id = request.data.get("teacher_id")
+        teacher = Users.objects.filter(id=teacher_id)
+
+
+        if not atendee or not teacher:
+            data = {"message": "Invalid Data"}
+            serializer = MessageSerializer(data)
+            return Response(serializer.data, status=status.HTTP_403_FORBIDDEN)
+        
+        if atendee[0].teacher_id != teacher.id:
+            data = {"message": "Invalid Data"}
+            serializer = MessageSerializer(data)
+            return Response(serializer.data, status=status.HTTP_403_FORBIDDEN)
+                
+        atendee[0].tchr_cfrm_released = datetime.now()
         atendee[0].save()
 
         data = {"message": "SuccessFully Updated"}
